14b490fde2246a04e7f2f77487563924a8d3dc26
juliettegonzalez <juliette.gonzalez@insa-rennes.fr>
2016-12-15 21:30:55 +0100

    Pawn selection button

--- app/src/main/java/com/juliettegonzalez/breakthroughapp/GameSelectionFragment.java
+++ app/src/main/java/com/juliettegonzalez/breakthroughapp/GameSelectionFragment.java
@@ -1,15 +1,18 @@
 package com.juliettegonzalez.breakthroughapp;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class GameSelectionFragment extends Fragment {
+
+    ImageButton mDragonPawnBtn, mGrandpaPawnBtn, mKingPawnBtn, mWizardPawnBtn;
 
     public GameSelectionFragment() {
     }
@@ -17,6 +20,24 @@ public class GameSelectionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_game_selection, container, false);
+        View view = inflater.inflate(R.layout.fragment_game_selection, container, false);
+
+        mDragonPawnBtn = (ImageButton) view.findViewById(R.id.dragon_pawn_btn);
+        mGrandpaPawnBtn = (ImageButton) view.findViewById(R.id.grandpa_pawn_btn);
+        mKingPawnBtn = (ImageButton) view.findViewById(R.id.king_pawn_btn);
+        mWizardPawnBtn = (ImageButton) view.findViewById(R.id.wizard_pawn_btn);
+
+        mDragonPawnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDragonPawnBtn.setBackgroundResource(R.drawable.white_rounded_button_selected);
+                mGrandpaPawnBtn.setBackgroundResource(R.drawable.white_rounded_button);
+                mKingPawnBtn.setBackgroundResource(R.drawable.white_rounded_button);
+                mWizardPawnBtn.setBackgroundResource(R.drawable.white_rounded_button);
+            }
+        });
+
+
+        return view;
     }
 }
