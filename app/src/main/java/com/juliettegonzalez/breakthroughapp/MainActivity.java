20b8954aa158dbd56a093d3261377104c447b6b1
juliettegonzalez <juliette.gonzalez@insa-rennes.fr>
2016-12-15 11:58:13 +0100

    First view

--- app/src/main/java/com/juliettegonzalez/breakthroughapp/MainActivity.java
+++ app/src/main/java/com/juliettegonzalez/breakthroughapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.juliettegonzalez.breakthroughapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        Typeface caviarDreams_bold = Typeface.createFromAsset(getAssets(), "fonts/CaviarDreams_Bold.ttf");
+        ((TextView)findViewById(R.id.title_text_view)).setTypeface(caviarDreams_bold);
+
+        getSupportActionBar().hide();
+
     }
 }
